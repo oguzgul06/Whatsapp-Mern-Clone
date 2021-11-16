1cd5d50fbf3dad394d98a06ead25270a1facde25
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-16 15:35:10 +0300

    Design to Chat-Header Side

--- wp-frontend/src/components/Chat.js
+++ wp-frontend/src/components/Chat.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Avatar, IconButton } from "@mui/material";
 import "./Chat.css";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 function Chat() {
   return (
     <div className="chat">
-      <h2>I'm the Chat</h2>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
