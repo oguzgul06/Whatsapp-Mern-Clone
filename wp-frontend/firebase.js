b6c5ba3508a906c27c9bcd82d3d6f609fb982f14
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-16 13:27:41 +0300

    Firebase config for authentication and hosting

---
+++ wp-frontend/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXhaeLSI8x2SuBv1zM0_UrRKYCbql23Us",
+  authDomain: "react-mern-whatsapp-2ca87.firebaseapp.com",
+  projectId: "react-mern-whatsapp-2ca87",
+  storageBucket: "react-mern-whatsapp-2ca87.appspot.com",
+  messagingSenderId: "335065952802",
+  appId: "1:335065952802:web:50b42e17e9af91ad36507b",
+};
