a7308f988b011c32219c07b3af613b430a1e5905
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-16 15:54:01 +0300

    Design to Chat-Body Side

--- wp-frontend/src/components/Chat.js
+++ wp-frontend/src/components/Chat.js
@@ -28,6 +28,26 @@ function Chat() {
           </IconButton>
         </div>
       </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Oguz</span>
+          This is the message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Oguz</span>
+          This is the message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Oguz</span>
+          This is the message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
     </div>
   );
 }
