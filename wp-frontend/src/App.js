ff0bac3633b90da79d1d4e0d7ed8bd88b9d84532
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-16 13:37:11 +0300

    Create Components(Sidebar, Chat)

--- wp-frontend/src/App.js
+++ wp-frontend/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Chat from "./components/Chat";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's Build Whatsapp Mern Clone</h1>
+      {/* Sidebar Component */}
+      <Sidebar />
+      {/* Chat Component */}
+      <Chat />
     </div>
   );
 }
---
+++ wp-frontend/src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chat.css";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <h2>I'm the Chat</h2>
+    </div>
+  );
+}
+
+export default Chat;
