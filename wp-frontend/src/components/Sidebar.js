3f2d8cfd1073cc3a02a1d6c65ba00087a0a693d2
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-16 14:33:22 +0300

    Design to Sidebar-Header

--- wp-frontend/src/components/Sidebar.js
+++ wp-frontend/src/components/Sidebar.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton } from "@mui/material";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>I'm the Sidebar</h2>
+      <div className="sidebar__header">
+        <Avatar src="https://avatars.githubusercontent.com/u/72518895?v=4" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
