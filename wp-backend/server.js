248b4863257ba22a71c3027324a6d8fba196a596
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-17 15:59:15 +0300

    Send post to message from postman

--- wp-backend/server.js
+++ wp-backend/server.js
@@ -1,6 +1,7 @@
 //importing
 import express from "express"; //ES6 notation
 import mongoose from "mongoose"; //ES6 notation
+import Messages from "./dbMessages.js";
 
 //app config
 const app = express();
@@ -12,14 +13,33 @@ const port = process.env.PORT || 9000;
 const connection_URL =
   "mongodb+srv://admin:admin@cluster0.timlc.mongodb.net/whatsappdb?retryWrites=true&w=majority";
 
-mongoose.connect(connection_URL, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  connection_URL,
+  {
+    //useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!!!");
+  }
+);
 
 //api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 //listening
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
