dbd670ae0a0f9cc2f01282cb1d905f9890305884
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-17 16:32:43 +0300

    Create pusher for push,fetch etc and Reconfig DB connection

--- wp-backend/server.js
+++ wp-backend/server.js
@@ -2,10 +2,19 @@
 import express from "express"; //ES6 notation
 import mongoose from "mongoose"; //ES6 notation
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 
 //app config
 const app = express();
 const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: "1298959",
+  key: "85677974eea15293d8c9",
+  secret: "dd8143c207543f2b1c08",
+  cluster: "ap2",
+  useTLS: true,
+});
 
 //middleware
 app.use(express.json());
@@ -14,18 +23,24 @@ app.use(express.json());
 const connection_URL =
   "mongodb+srv://admin:admin@cluster0.timlc.mongodb.net/whatsappdb?retryWrites=true&w=majority";
 
-mongoose.connect(
-  connection_URL,
-  {
-    //useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log("Connected to MongoDB!!!");
-  }
-);
+mongoose.connect(connection_URL, {
+  //useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(change);
+  });
+});
 
 //api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
