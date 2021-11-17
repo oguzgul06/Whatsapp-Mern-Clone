92cdfaddd3b6b376f745067c21935cec4686ca77
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-17 15:21:57 +0300

    Configuration MongoDB

--- wp-backend/server.js
+++ wp-backend/server.js
@@ -1,11 +1,25 @@
 //importing
+import express from "express"; //ES6 notation
+import mongoose from "mongoose"; //ES6 notation
 
 //app config
+const app = express();
+const port = process.env.PORT || 9000;
 
 //middleware
 
 //DB config
+const connection_URL =
+  "mongodb+srv://admin:admin@cluster0.timlc.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+
+mongoose.connect(connection_URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
 //listening
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
