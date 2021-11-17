2f485fcc71157ce7b62202d8d1cd20e45550c5bf
Oğuz GÜL <ogzgl06@gmail.com>
2021-11-17 16:12:37 +0300

    Create ModgoDBSchema for model and create api routes with middlaware

--- wp-backend/server.js
+++ wp-backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 //middleware
+app.use(express.json());
 
 //DB config
 const connection_URL =
@@ -28,6 +29,16 @@ mongoose.connect(
 
 //api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
